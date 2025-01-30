16fed8525cf8f10dd75ee193237459bc15c9629c
3relos <99477598+3relos@users.noreply.github.com>
2025-01-30 14:27:02 +0100

    Update get_handins.sh
    Changed all texts and comments to English.
    Exchanged real path with dummy path and removed Username.

--- get_handins.sh
+++ get_handins.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-# Zahl abfragen
-read -p "Übungsblatt Nr. eingeben: " zahl
+# Created: Vasco (3relos)
+
+# number abfragen
+read -p "Enter exercise sheet no.: " number
 
 # Source directory with the PDF-files
-SOURCE_DIR="/Users/3relos/Downloads/Uebungsblatt$zahl"
+SOURCE_DIR="/Users/<Username>/Downloads/Uebungsblatt$number"
 # Destiny directory, where the PDF-files should be copied
-DEST_DIR="/Users/3relos/sciebo/Uni/Bachelor/Theo/Tutorium_WS24-25/Abgaben/ÜB$zahl"
+DEST_DIR="/Users/<Username>/Tutorium_WS24-25/Abgaben/Ex$number"
 
 # Check if the source dir exists
-[ -d "$SOURCE_DIR" ] || { echo "Quellverzeichnis existiert nicht: $SOURCE_DIR"; exit 1; }
+[ -d "$SOURCE_DIR" ] || { echo "Source directory does not exist: $SOURCE_DIR"; exit 1; }
 
 # Check if the destiny dir exists, if not create it
-[ -d "$DEST_DIR" ] || { echo "Zielverzeichnis existiert nicht. Erstelle: $DEST_DIR"; mkdir -p "$DEST_DIR"; }
+[ -d "$DEST_DIR" ] || { echo "Target directory does not exist. Create: $DEST_DIR"; mkdir -p "$DEST_DIR"; }
 
 # Searches for all PDF-files in the source dir and copy it to the destiny dir
 find "$SOURCE_DIR" -type f -iname "*.pdf" -exec cp {} "$DEST_DIR" \;
 
-echo "All PDF-files were copied to 'ÜB$zahl'."
+echo "All PDF-files were copied to '$DEST_DIR'."
